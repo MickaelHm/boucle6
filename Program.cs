ae1ee56d1b1bae812655541d451d962a8770fab6
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 12:51:12 +0100

    complet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucle6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] months = new string[] { "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+            foreach (string month in months)
+            {
+                Console.WriteLine(month);
+            }
+        }
+    }
+}
